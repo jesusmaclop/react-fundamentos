6dfd0de925456fcceb1941e9219365fa25c07149
jesusmaclop <jmacias@itexico.net>
2019-01-07 15:45:54 -0600

    fundamentos1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hola Mundo 
+      </div>
+    );
+  }
+}
+
+export default App;
